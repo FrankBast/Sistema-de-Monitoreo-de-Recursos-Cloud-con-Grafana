9506003a18e511febf5e9763bc547c26e24aa6c2
Franklin Aguirre <franstuard15@gmail.com>
2026-02-17 10:27:50 -0600

    Add files via upload

---
+++ infra/terraform/aws/environments/dev/variables.tf
@@ -0,0 +1,11 @@
+# Enlace simbolico de ../aws/variables.tf
+# igual para cada modulo
+
+backend.tf
+locals.tf -> ../../locals.tf
+main.tf
+outputs.tf
+providers.tf -> ../../providers.tf
+terraform.tfvars
+variables.tf -> ../../variables.tf
+versions.tf -> ../../versions.tf
