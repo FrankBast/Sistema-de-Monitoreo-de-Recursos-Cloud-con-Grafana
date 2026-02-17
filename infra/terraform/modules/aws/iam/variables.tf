6ef768c6df1c8f2db7769e0900362f2d0b0c2329
Franklin Aguirre <franstuard15@gmail.com>
2026-02-17 11:13:13 -0600

    Add files via upload

---
+++ infra/terraform/modules/aws/iam/variables.tf
@@ -0,0 +1,8 @@
+# ==============================================================================
+# VARIABLES DEL MÓDULO IAM (ANSD)
+# ==============================================================================
+
+variable "project_name" {
+  description = "Nombre del proyecto para prefijar roles y perfiles"
+  type        = string
+}
