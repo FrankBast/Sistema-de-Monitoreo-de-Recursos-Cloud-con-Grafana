9d5200968c544b9b938f1b5dc77903cedbcb0646
Franklin Aguirre <franstuard15@gmail.com>
2026-02-17 10:50:35 -0600

    Define variables for visuals module
    Added variables for project name and instance details.

--- infra/terraform/modules/aws/visuals/variables.tf
+++ infra/terraform/modules/aws/visuals/variables.tf
@@ -1,1 +1,19 @@
+# ==============================================================================
+# VARIABLES DEL MÓDULO VISUALS
+# ==============================================================================
 
+variable "project_name" {
+  description = "Nombre del proyecto para prefijos y etiquetas"
+  type        = string
+}
+
+variable "instance_details" {
+  description = "Mapa con los IDs y detalles de las instancias a monitorear"
+  # Debe coincidir con la estructura que sale del módulo compute
+  type = map(object({
+    id         = string
+    private_ip = string
+    public_ip  = string
+    role       = string
+  }))
+}
